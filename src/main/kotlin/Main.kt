e4f7ae6221db441fa4f344badd9f1adf503a6bc8
Jack Richard <me@jackrichard.dev>
2022-05-17 15:39:22 -0400

    Rewrite now functioning!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,104 +1,9 @@
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-/*
-interface NodeBuilder
-
-abstract class Node {
-    abstract val id: String
-    abstract val position: Pair<Int, Int>
-    var children: List<Node> = listOf()
-    var parentNode: Node? = null
-    val metadata: NodeMetadata? = null
-
-    class BasicNodeBuilder : NodeBuilder {
-        internal val newChildren = mutableListOf<Node>()
-        operator fun Node.unaryPlus() = newChildren.add(this)
-    }
-
-    open fun build(init: BasicNodeBuilder.() -> Unit) : Node {
-        val builder = BasicNodeBuilder().also(init)
-        children = builder.newChildren
-        children.forEach { it.parentNode = this }
-        return this
-    }
-}
-
-data class NodeMetadata(val building: Char, val floor: Int, val room: Int) {
-    fun getIdentifier() : String = "$building$floor${(if (room < 10) "0" else "") + room.toString()}"
-}
-
-class FloorNode(override val id: String, override val position: Pair<Int, Int>) : Node()
-
-class DirectionalNode(override val id: String, override val position: Pair<Int, Int>) : Node() {
-    class DirectionalNodeBuilder : NodeBuilder {
-        val newChildren = mutableListOf<Node>()
-
-    }
-
-    override fun build(init: DirectionalNodeBuilder.() -> Unit): Node {
-
-    }
-}
-
-class RoomNode(override val id: String, override val position: Pair<Int, Int>) : Node()
-class ElevatorNode(override val id: String, override val position: Pair<Int, Int>) : Node()
-class StairNode(override val id: String, override val position: Pair<Int, Int>) : Node()
-
-fun main() {
-    val room1 = RoomNode("r1", 0 to 0)
-    val room2 = RoomNode("r2", 0 to 0)
-    val room3 = RoomNode("r3", 0 to 0)
-    val room4 = RoomNode("r4", 0 to 0)
-
-    val hallway1 = DirectionalNode("h1", 0 to 0)
-    val hallway2 = DirectionalNode("h2", 0 to 0)
-    val hallway3 = DirectionalNode("h3", 0 to 0)
-
-    val elevator1 = ElevatorNode("e1", 0 to 0)
-    val elevator2 = ElevatorNode("e2", 0 to 0)
-
-    val stairs1 = StairNode("s1", 0 to 0)
-    val stairs2 = StairNode("s2", 0 to 0)
-
-    val floor1 = FloorNode("f1", 0 to 0).build {
-        +hallway1.build {
-            +room1
-            +elevator1.build { +elevator2 }
-            +stairs1.build { +stairs2 }
-        }
-        +hallway2.build {
-            +room2
-            +room3
-        }
-    }
-
-    val floor2 = FloorNode("f2", 0 to 0).build {
-        +hallway3.build {
-            +room4
-            +elevator2.build { +elevator1 }
-            +stairs2.build { +stairs1 }
-        }
-    }
-
-    fun recursionStuff(node: Node?, destination: Node, path: List<Node> = listOf(node!!), max: Int = 0) : Node? {
-        return if (max < 16 && node != null && (path.size < 2 || !path.take(path.size - 1).contains(node))) {
-            if (node.id == destination.id) { println(path.joinToString { "Go to " + it.id }); return destination }
-            val b = node.children.mapNotNull { recursionStuff(it, destination, path + it, max + 1) }
-            return if (node.parentNode != null) { recursionStuff(node.parentNode, destination, path + node.parentNode!!,max + 1) ?: b.firstOrNull() } else b.firstOrNull()
-        } else null
-    }
-
-    println(if (recursionStuff(room1, room4)?.id == room4.id) "Destination reached!" else "Destination not found.")
-
-    embeddedServer(Netty, port = 80) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!")
-            }
-        }
-    }.start(wait = true)
-}*/
+const val coordXScale = 1.0
+const val coordYScale = 1.0
+const val maxSteps = 16
 
 data class Metadata(val building: Char, val floor: Int, val room: Int) {
     fun getIdentifier() : String = "$building$floor${(if (room < 10) "0" else "") + room.toString()}"
@@ -106,14 +11,12 @@ data class Metadata(val building: Char, val floor: Int, val room: Int) {
 
 enum class Direction { NORTH, EAST, WEST, SOUTH }
 
-data class DirectionNode(
-    val north: List<DirectionNode> = listOf(),
-    val east: List<DirectionNode> = listOf(),
-    val west: List<DirectionNode> = listOf(),
-    val south: List<DirectionNode> = listOf(),
+class DirectionNode(
+    private val neighbors: MutableList<DirectionNode> = mutableListOf(),
+    val id: String,
     val position: Pair<Double, Double>
 ) {
-    fun distanceTo(other: DirectionNode) : Double = sqrt((other.position.first - this.position.first).pow(2) + (other.position.second - this.position.second).pow(2))
+    fun distanceTo(other: DirectionNode) : Double = sqrt((other.position.first - this.position.first).times(coordXScale).pow(2) + (other.position.second - this.position.second).times(coordYScale).pow(2))
     fun directionTo(other: DirectionNode) : Direction = when {
             this.position.first > other.position.first -> Direction.WEST
             this.position.first < other.position.first -> Direction.EAST
@@ -121,11 +24,52 @@ data class DirectionNode(
             this.position.second < other.position.second -> Direction.NORTH
             else -> throw Exception("Nodes are at the same position!")
         }
+
+    fun getNeighbors() = neighbors
+    fun addNeighbor(other: DirectionNode) {
+        this.neighbors.add(other)
+        other.neighbors.add(this)
+    }
+
+    fun paths(to: DirectionNode) : List<DirectionNode> {
+        fun getAllPathsR(
+            from: DirectionNode,
+            to: DirectionNode,
+            step: Int = 0,
+            path: List<DirectionNode> = listOf()
+        ): List<DirectionNode>? {
+            if (from == to || step > maxSteps) {
+                return path
+            }
+            return from.neighbors.filter { !path.contains(it) }
+                .firstNotNullOfOrNull { getAllPathsR(it, to, step + 1, path + it) }
+        }
+
+        return this.neighbors.mapNotNull { getAllPathsR(it, to) }.minByOrNull { it.size } ?: emptyList()
+    }
+
+    override fun toString(): String = id
 }
 
+fun mapDirections(path: List<DirectionNode>) : List<String> =
+    path.take(path.size - 1).mapIndexed { i, n ->
+        "Go " + n.directionTo(path[i + 1]) + " for " + n.distanceTo(path[i + 1]) + " feet towards " + path[i + 1] + "."
+    } + "Arrive at ${path.last()}!"
+
 fun main() {
-    val n1 = DirectionNode(position = -1.0 to 0.0)
-    val n2 = DirectionNode(position = -1.0 to 1.0)
-    println(n1.distanceTo(n2))
-    println(n1.directionTo(n2))
+    val n1 = DirectionNode(position = -1.0 to 0.0, id = "1")
+    val n2 = DirectionNode(position = 0.0 to 0.0, id = "2")
+    val n3 = DirectionNode(position = 0.0 to 1.0, id = "3")
+    val n4 = DirectionNode(position = 1.0 to 1.0, id = "4")
+    val n5 = DirectionNode(position = 0.5 to 1.0, id = "5")
+    val n6 = DirectionNode(position = 1.0 to 0.5, id = "6")
+
+    n1.addNeighbor(n2)
+    n2.addNeighbor(n3)
+    n3.addNeighbor(n4)
+    n2.addNeighbor(n5)
+    n5.addNeighbor(n6)
+    n6.addNeighbor(n4)
+
+    println(mapDirections(n1.paths(n4)).joinToString("\n"))
 }
